85e8ba1076688f7764d9790568fc267929d284c7
atendente <atendente@PM31P30.SPEEDMAIS.LOCAL>
2025-05-11 15:07:10 -0300

    listas

---
+++ lista.py
@@ -0,0 +1,11 @@
+minha_lista = []
+
+
+minha_lista.append("crepusculo ")
+minha_lista.append("lua nova")
+print("Depois de adicionar:", minha_lista)
+
+
+minha_lista.remove("crepusculo ")
+print("Depois de remover:", minha_lista)
+
